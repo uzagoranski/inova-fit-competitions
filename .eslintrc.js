d9c23d9fbbad62160072450994e552b4cfb511a3
Uros Zagoranski <uros.zagoranski@inova.si>
2019-09-09 09:21:17 +0200

    Lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports =  {
         "@typescript-eslint/camelcase": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "no-plusplus": "off",
-        "array-callback-return": "off"
+        "array-callback-return": "off",
+        "no-console": "off"
       } 
 };
